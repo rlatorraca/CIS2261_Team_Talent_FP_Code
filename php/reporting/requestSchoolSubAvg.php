5620848116206cabeaee794e6f7a790b5ddc8ba2
Sara Courtney <scourtney@hollandcollege.com>
2019-01-28 19:45:23 -0400

    Dropdown box population!

--- php/reporting/requestSchoolSubAvg.php
+++ php/reporting/requestSchoolSubAvg.php
@@ -1,64 +1,77 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sahra
- * Date: 2019-01-27
- * Time: 8:58 PM
- */
+    /**
+     * Created by PhpStorm.
+     * User: sahra
+     * Date: 2019-01-27
+     * Time: 8:58 PM
+     */
 ?>
 <!--Form to request to view a schoolS' average in a subject
 Requires user, user id, user school, school id, subjects, courses associated to subjects, grades, years & semester-->
 
-<!--//Create a new connection object using mysqli-->
 
-                    <?php
+<?php
 
-                    //*****************attempting dropdown of school subjects**********************************************************
+    //*****************attempting dropdown of school subjects**********************************************************
+    //Get schoolID of user
+    //$schoolID = ($_GET['schoolID']);
 
-                    //Create a new connection object using mysqli
-                    @
-                    $db = new mysqli('localhost', 'root', '', 'stars');
+    //Deter SQL injection
+    //$cleanSchoolID = $db->real_escape_string($schoolID);
+    //echo "got here1";
 
-                    echo "got here0";
+    //Create a new connection object using mysqli
+    //Make connection to the database and check to ensure that a solid connection can be made
+    @ $database = new mysqli('localhost', 'root', '', 'stars');
+    if (mysqli_connect_errno()) {
+        echo '<h2>An error has occurred.  Would you like to <a href=\'requestSchoolSubAvg.php\'>try again?</a></h2>';
+        exit("</div></body></html>");
+        $db->close();
+    }
 
-                    //if cannot connect to database, display message to user & close db
-                    if (mysqli_connect_errno()) {
-                        echo 'Error: Could not connect to database.  Please try again later.</body></html>';
-                        $db->close();
-                        exit;
-                    }
+    $query = "SELECT name FROM school WHERE schoolID > 1";
 
-//                    //Get schoolID of user
-//                    $schoolID = ($_GET['schoolID']);
-//
-//                    //Deter SQL injection
-//                    $cleanSchoolID = $db->real_escape_string($schoolID);
-//                    echo "got here1";
+    //Execute query and store result.
+    $result = $database->query($query);
 
-                    //The SQL query to populate the dropdown of subjects based on schoolID
-                    if ($result) {
-                        $query = "SELECT subject.subjectName FROM subject, school WHERE school.schoolID = 69";
-                        echo "got here2";
-                    } else {
-                        //if cannot connect to db let user know, offer link back to ??????????????????
-                        echo "An error has occurred.  Would you like to <a href='requestSchoolSubAvg.php'>try again?</a>";
+    if ($result != null) {
+        // output data of row
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "name: " . $row["name"]. " <br>";
+            $subject = $row["name"];
+        }
+    } else {
+        echo "0 results";
+    };
 
-                        $db->close();
-                    }
-                    var_dump($query);
+    echo "<select name=student value=''>School</option>"; // list box select command
 
-                    $result = $db->query($query);
-                    echo "got here3";
-                    //Get the num_rows attribute of the $result object
-                    //This is key to knowing if we should show the results or display an error message etc
-                    $num_results = $result->num_rows;
+    foreach ($database->query($query) as $row){//Array or records stored in $row
 
-                    //echo "<p>Total Results: $num_results</p>";
+        echo "<option value=$row[id]>$row[name]</option>";
 
-                    //Check/validate if there are items in the database object
-                    if ($result->num_rows > 0)
+        /* Option values are added by looping through the array */
 
-                        ?>
+    }
+
+    echo "</select>";// Closing of list box
+
+
+
+
+    echo "got here4";
+    //Get the num_rows attribute of the $result object
+    //This is key to knowing if we should show the results or display an error message etc
+    $num_results = $result->num_rows;
+
+    echo "<p>Total Results: $num_results</p>";
+
+    //Check/validate if there are items in the database object
+    if ($result->num_rows > 0) {
+        echo "wow";
+    }
+
+?>
 
 <form action="displaySchoolSubAvg.php" method="post">
     <div class="form-group">
@@ -66,9 +79,9 @@ Requires user, user id, user school, school id, subjects, courses associated to 
             <div class="col-3">
 
                 <!--populate a header? with the name of the school associated with the user.  Use that hidden ID to write SQL-->
-<!---->
-<!--                <input type="hidden" class="form-control" id="isbn" name="isbn"-->
-<!--                       value="--><?php //echo $row['isbn'] ?><!--">-->
+                <!---->
+                <!--                <input type="hidden" class="form-control" id="isbn" name="isbn"-->
+                <!--                       value="--><?php //echo $row['isbn'] ?><!--">-->
 
 
             </div>
@@ -94,29 +107,29 @@ Requires user, user id, user school, school id, subjects, courses associated to 
                 <select class="g" id="selectYearBegin" name="selectYearBegin">
                     <!-- Here we need to use SQL queries to populate the dropdowns -->
                     <?php
-                    for ($i = 2016; $i <= 2020; $i++) {
-                        ?>
-                        <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                        <?php
-                    }
+                        for ($i = 2016; $i <= 2020; $i++) {
+                            ?>
+                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                            <?php
+                        }
                     ?>
                 </select>
                 <select class="g" id="selectYearEnd" name="selectYearEnd">
                     <!-- Here we need to use SQL queries to populate the dropdowns -->
                     <?php
-                    for ($i = 2016; $i <= 2020; $i++) {
-                        ?>
-                        <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                        <?php
-                    }
+                        for ($i = 2016; $i <= 2020; $i++) {
+                            ?>
+                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                            <?php
+                        }
                     ?>
                 </select>
             </div>
 
             <!-- Button elements declared here. Button includes is above with button object declared. !-->
-<!--            --><?php
-//            $confirm->buttonName = "Submit";
-//            $confirm->buttonValue = "Request";
-//            $confirm->buttonStyle = "font-family:sans-serif";
-//            $confirm->display(); ?>
+            <!--            --><?php
+                //            $confirm->buttonName = "Submit";
+                //            $confirm->buttonValue = "Request";
+                //            $confirm->buttonStyle = "font-family:sans-serif";
+                //            $confirm->display(); ?>
 </form>
