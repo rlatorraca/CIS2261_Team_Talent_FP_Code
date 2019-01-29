362f7200f7fdb3f3d1c9ac8b97dc640d84cc4b47
HC\jgaudet109873 <Jgaudet109873@hollandcollege.com>
2019-01-29 16:01:42 -0400

    More changes.  Report Card sql works.

--- php/reporting/displayReportCard.php
+++ php/reporting/displayReportCard.php
@@ -18,15 +18,15 @@
     <?php
 
     //Variable to hold the student ID requested
-    $studentID = 1;
-
+    $studentID = 12;// should this be 12? Yes lol.
     //Make connection to the database and check to ensure that a solid connection can be made
     @ $database = new mysqli('localhost', 'root', '', 'stars');
         if (mysqli_connect_errno()) {
             echo '<h2>Error: Could not connect to database. Please try again later.</h2>';
             exit("</div></body></html>");
         }
-
+        // you have a look and see what you can find. Check in again ina  few
+        //Thanks!
         //SQL to pull overall report card details (Report card to identify a student before the extraction of enrollment data).
         //Be sure to pull the selected student from the requestReportCard.php page to extract the correct student information
         $query1 = "SELECT * FROM reportcard WHERE studentID = '$studentID';";
@@ -70,12 +70,11 @@
         } else {
             echo "<p>No results</p>";
         }
-
-        //SQL query to pull all enrollment data for a student (Be sure to again use the studentID pulled from the request page)
-        $query2 = "SELECT enrollment.enrollmentID, course.courseName, subject.subjectCode, mark, attendance, 
-                    notes, educator.firstName, educator.lastName 
-                    FROM student, enrollment, course, courseoffering, subject, educator 
-                    WHERE studentID = '$studentID' 
+            //SQL for individual Enrollments
+        $query2 = "SELECT enrollment.enrollmentID, course.courseName, subject.subjectCode, mark, attendance,
+                    notes
+                    FROM student, enrollment, course, courseoffering, subject, semester
+                    WHERE enrollment.studentID = '$studentID'
                     AND student.studentID = enrollment.studentID
                     AND enrollment.schoolYear = '$schoolYear'
                     AND enrollment.semesterNum = '$semesterNum'
@@ -83,8 +82,7 @@
                     AND semester.semesterNum = enrollment.semesterNum
                     AND course.subjectCode = subject.subjectCode
                     AND courseoffering.courseID = course.courseID
-                    AND enrollment.classID = courseoffering.classID
-                    AND courseoffering.educatorID = educator.educatorID;";
+                    AND enrollment.classID = courseoffering.classID;";
 
         $result2 = $database->query($query2);
 
@@ -100,7 +98,6 @@
                     <td>Mark</td>
                     <td>Days Missed</td>
                     <td>Teacher Notes</td>
-                    <td>Teacher Name</td>
                 </tr>
                 </thead>
             <?php
@@ -112,10 +109,9 @@
                         <td> <?php echo $row["enrollmentID"]; ?></td>
                         <td> <?php echo $row["courseName"]; ?></td>
                         <td> <?php echo $row["subjectCode"]; ?></td>
-                        <td> <?php echo $row["mark"]; ?></td>
-                        <td> <?php echo $row["attendance"]; ?></td>
-                        <td> <?php echo $row["notes"]; ?></td>
-                        <td> <?php echo $row["educator.firstName"]." ".$row["educator.lastName"]; ?></td>
+                        <td> <?php if ($row["mark"] == ""){ echo "Empty"; } else { echo $row["mark"]; } ?></td>
+                        <td> <?php if ($row["attendance"] == "") { echo "0 days"; } else { echo $row["attendance"]; } ?></td>
+                        <td> <?php if ($row["notes"] == "") { echo "Empty"; } else { echo $row["notes"]; } ?></td>
                     </tr>
                 </tbody>
                 <?php } ?>
@@ -124,6 +120,8 @@
         } else {
             echo "<p>No results</p>";
         }
+
+
         ?>
 </body>
 </html>
