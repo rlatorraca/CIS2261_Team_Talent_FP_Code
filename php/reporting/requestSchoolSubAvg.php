546cf39944203fb55fd6232115f56aed0ef4f2a8
Sara Courtney <scourtney@hollandcollege.com>
2019-01-29 15:39:56 -0400

    More SQL queries

--- php/reporting/requestSchoolSubAvg.php
+++ php/reporting/requestSchoolSubAvg.php
@@ -7,7 +7,6 @@
      */
 ?>
 <!--Form to request to view a schoolS' average in a subject
-Requires user, user id, user school, school id, subjects, courses associated to subjects, grades, years & semester-->
 
 
 <?php
@@ -28,86 +27,103 @@ Requires user, user id, user school, school id, subjects, courses associated to 
         $db->close();
     }
     //create the query
-    $query = "SELECT subject.subjectName FROM subject, school WHERE school.schoolID = 69";
-   // $query = "SELECT name FROM school WHERE schoolID > 1";
-
+    $querySubject = "SELECT subject.subjectName FROM subject, school WHERE school.schoolID = 69";
+    $queryYear = "SELECT DISTINCT schoolYear FROM `semester`";;
     //Execute query and store result.
-    $result = $database->query($query);
-    //if no results
-    if ($result->num_rows < 1) {
-        echo "<label for='course'>Select Course</label>
-           <select name=student ></option>";
-        {
-            echo "<option value=''>No Results</option>";
-        }
-        echo "</select>";
-        //if results exist
-    } else if ($result) {
-
-        //while ($row = mysqli_fetch_assoc($result)) {
-        echo "<label for='course'>Select Subject</label>
-           <select name=student value=''>"; // list box select command
-
-        foreach ($database->query($query) as $row) {//Array or records stored in $row
-
-            echo "<option value=$row[id]>$row[name]</option>";
-            /* Option values are added by looping through the array */
-        }
-        echo "</select>";// Closing box
-    }
+    $resultSubject = $database->query($querySubject);
+    $resultYear = $database->query($queryYear);
 ?>
 
 <form action="displaySchoolSubAvg.php" method="post">
     <div class="form-group">
         <div class="form-row">
             <div class="col-3">
-
                 <!--populate a header? with the name of the school associated with the user.  Use that hidden ID to write SQL-->
                 <!---->
                 <!--                <input type="hidden" class="form-control" id="isbn" name="isbn"-->
                 <!--                       value="--><?php //echo $row['isbn'] ?><!--">-->
 
+            </div>
+            <div class="col-3">
+                <!--Check to see what years data exists for the chosen subject....-->
+                <?php
+                    if ($resultSubject->num_rows < 1) {
+                        echo "<label for='course'>Select Course</label>
+                        <select name=student ></option>";
+                        {
+                            echo "<option value=''>No Results</option>";
+                        }
+                        echo "</select>";
+                        //if results exist
+                    } else if ($resultSubject) {
+                        //while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<label for='course'>Select Subject</label>
+                        <select name=student value=''>"; // list box select command
+
+                        foreach ($database->query($querySubject) as $row) {//Array or records stored in $row
+
+                            echo "<option value=$row[subjectCode]>$row[subjectName]</option>";
+                            /* Option values are added by looping through the array */
+                        }
+                        echo "</select>";
+                    }
+                ?>
+            </div>
+            <div class="col-3">
+                <!--                <label for="semester">Select Year Range</label>-->
+                <br>
+                <!--                <select class="g" id="selectYearBegin" name="selectYearBegin">-->
+                <!-- Here we need to use SQL queries to populate the dropdowns -->
+                <?php
+                    if ($resultYear->num_rows < 1) {
+                        echo "<label for='course'>Beginning</label><br>
+                        <select id='yearBegin' name='yearBegin' ></option>";
+                        {
+                            echo "<option value=''>No Results</option>";
+                        }
+                        echo "</select>";
+                        //if results exist
+                    } else if ($resultYear) {
+                        //while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<label for='course'>Beginning</label><br>
+                        <select id='yearEnd' name='yearEnd'>"; // list box select command
+
+                        foreach ($database->query($queryYear) as $row) {//Array or records stored in $row
+
+                            echo "<option value=$row[schoolYear]>$row[schoolYear]</option>";
+                            /* Option values are added by looping through the array */
+                        }
+                        echo "</select>";
+                    }
+                ?>
 
             </div>
             <div class="col-3">
-                <label for="selectSubject">Select Subject</label>
-                <br>
-                <select class="g" id="selectSubject" name="selectSubject">
-                    <!-- Here we need to use SQL queries to populate the dropdown with available subjects -->
-                    <?php
-                        //*****************************************************************************
+                <!--                <select class="g" id="selectYearEnd" name="selectYearEnd">-->
+                <!-- Here we need to use SQL queries to populate the dropdowns -->
+                <?php
+                    if ($resultYear->num_rows < 1) {
+                        echo "<label for='course'>Endg</label><br>
+                        <select name=student ></option>";
+                        {
+                            echo "<option value=''>No Results</option>";
+                        }
+                        echo "</select>";
+                        //if results exist
+                    } else if ($resultYear) {
+                        //while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<label for='course'>End</label><br>
+                        <select name=student value=''>"; // list box select command
 
-                        for ($i = 2016; $i <= 2020; $i++) {
-                            ?>
-                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                            <?php
+                        foreach ($database->query($queryYear) as $row) {//Array or records stored in $row
+
+                            echo "<option value=$row[schoolYear]>$row[schoolYear]</option>";
+                            /* Option values are added by looping through the array */
                         }
-                    ?>
-                </select>
-            </div>
-            <div class="col-3">
-                <label for="semester">Select Year Range</label>
-                <br>
-                <select class="g" id="selectYearBegin" name="selectYearBegin">
-                    <!-- Here we need to use SQL queries to populate the dropdowns -->
-                    <?php
-                        for ($i = 2016; $i <= 2020; $i++) {
-                            ?>
-                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                            <?php
-                        }
-                    ?>
-                </select>
-                <select class="g" id="selectYearEnd" name="selectYearEnd">
-                    <!-- Here we need to use SQL queries to populate the dropdowns -->
-                    <?php
-                        for ($i = 2016; $i <= 2020; $i++) {
-                            ?>
-                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                            <?php
-                        }
-                    ?>
-                </select>
+                        echo "</select>";
+                    }
+                ?>
+
             </div>
 
             <!-- Button elements declared here. Button includes is above with button object declared. !-->
