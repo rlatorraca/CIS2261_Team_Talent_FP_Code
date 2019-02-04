a50bb7a4adfa74ddec20ecca2312b2e526045d08
HC\jgaudet109873 <Jgaudet109873@hollandcollege.com>
2019-02-04 10:15:47 -0400

    Changes to edit student.

--- php/admin/editStudent.php
+++ php/admin/editStudent.php
@@ -10,14 +10,16 @@ include "../db/dbConn.php";
 
 if (isset($_POST["update"])) {
 
-
+    $firstNameFromForm = $_POST["firstName"];
+    $middleNameFromForm = $_POST["middleName"];
+    $lastNameFromForm = $_POST["lastName"];
 
 } else {
 
 //Get studentID from list page of students
-    $studentID = 7;
+    $studentID = 3;
 
-    $queryStudent = "SELECT * FROM student WHERE studentID = 7;";
+    $queryStudent = "SELECT * FROM student WHERE studentID = $studentID;";
 
     $resultSetFromQueryStudent = $database->query($queryStudent);
 
@@ -42,83 +44,83 @@ if (isset($_POST["update"])) {
 
     ?>
     <!doctype html>
-            <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport"
-                          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-                    <!-- Fonts !-->
-                    <link href="https://fonts.googleapis.com/css?family=Archivo+Black|Roboto" rel="stylesheet">
+        <!-- Fonts !-->
+        <link href="https://fonts.googleapis.com/css?family=Archivo+Black|Roboto" rel="stylesheet">
 
-                    <!--Link to custom style sheet-->
-                    <link href="../../css/stars.css" rel="stylesheet">
+        <!--Link to custom style sheet-->
+        <link href="../../css/stars.css" rel="stylesheet">
 
-                    <!-- JQuery Links !-->
-                    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-                    <link rel="stylesheet" href="/resources/demos/style.css">
-                    <!-- JQuery Calendar Date Picker !-->
-                    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-                    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <!-- JQuery Links !-->
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/resources/demos/style.css">
+        <!-- JQuery Calendar Date Picker !-->
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
-                    <!-- Here is where we call bootstrap. !-->
-                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-                            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-                            crossorigin="anonymous"></script>
+        <!-- Here is where we call bootstrap. !-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+                integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+                crossorigin="anonymous"></script>
 
-                    <!--function to go back to your incomplete form without losing previously filled fields-->
-                    <script>
-                        function goBack() {
-                            window.history.back();
-                        }
+        <!--function to go back to your incomplete form without losing previously filled fields-->
+        <script>
+            function goBack() {
+                window.history.back();
+            }
 
-                        // This function shows the date picker.
-                        $(function () {
-                            $("#datepicker").datepicker();
-                        });
+            // This function shows the date picker.
+            $(function () {
+                $("#datepicker").datepicker();
+            });
 
-                        // This function shows the note.
-                        // Will need to add a variable to get the notes to then call.
-                        $(function () {
-                            $(document).tooltip();
-                        });
+            // This function shows the note.
+            // Will need to add a variable to get the notes to then call.
+            $(function () {
+                $(document).tooltip();
+            });
 
-                        // This function manages the drop downs on the main menu.
-                        $(function () {
-                            $("#menu").menu();
-                        });
-                    </script>
-                    <title>STARS - Edit Student</title>
-        </head>
-        <body>
-            <div class="header">
-                <img src="../../img/StarsWhiteFIN.jpg">
+            // This function manages the drop downs on the main menu.
+            $(function () {
+                $("#menu").menu();
+            });
+        </script>
+        <title>STARS - Edit Student</title>
+    </head>
+    <body>
+    <div class="header">
+        <img src="../../img/StarsWhiteFIN.jpg">
+    </div>
+    <div class="jumbotron-fluid">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-sm-6">Testing</div>
+                <div class="col-sm-6">Testing 2</div>
+
             </div>
-            <div class="jumbotron-fluid">
-                <div class="container-fluid">
-
-                    <div class="row">
-                        <div class="col-sm-6">Testing</div>
-                        <div class="col-sm-6">Testing 2</div>
-
-                    </div>
 
             <form action="editStudent.php" method="post">
-                    <h2>Student Details</h2>
-                    <p>**Please ensure all fields are filled before editing a Student's details.</p>
-                    <div class = "row">
+                <h2>Student Details</h2>
+                <p>**Please ensure all fields are filled before editing a Student's details.</p>
+                <div class="row">
                     <div class="col-sm-6">
                         <label for="studentID">Student ID</label>
-                        <input type="text" name="studentID" class="form-control" value="7">
+                        <input type="text" name="studentID" class="form-control" value="<?php echo $studentID; ?>" readonly>
                     </div>
                     <div class="col-sm-6">
                         <label for="firstname">First Name</label>
                         <input type="text" name="firstName" class="form-control"
                                value="<?php echo $firstName; ?>">
                     </div>
-                    </div>
-                <div class = "row">
+                </div>
+                <div class="row">
                     <div class="col-sm-6">
                         <label for="middlename">Middle Name</label>
                         <input type="text" name="middleName" class="form-control"
@@ -130,7 +132,7 @@ if (isset($_POST["update"])) {
                                value="<?php echo $lastName; ?>">
                     </div>
                 </div>
-                <div class = "row">
+                <div class="row">
                     <div class="col-sm-6">
                         <label for="title">Gender</label>
                         <input type="text" name="gender" class="form-control"
@@ -143,7 +145,7 @@ if (isset($_POST["update"])) {
                                value="<?php echo $dob; ?>">
                     </div>
                 </div>
-                <div class = "row">
+                <div class="row">
                     <div class="col-sm-6">
                         <label for="title">Grade</label>
                         <select name="grade">
@@ -158,7 +160,7 @@ if (isset($_POST["update"])) {
                                value="<?php echo $address; ?>">
                     </div>
                 </div>
-                <div class = "row">
+                <div class="row">
                     <div class="col-sm-6">
                         <label for="title">Phone Number</label>
                         <input type="text" name="phoneNum" class="form-control"
@@ -167,17 +169,17 @@ if (isset($_POST["update"])) {
                     <div class="col-sm-6">
                         <label for="title">Email Address</label>
                         <input type="text" name="emailAddress" form-control"
-                               value="<?php echo $emailAddress; ?>">
+                        value="<?php echo $emailAddress; ?>">
                     </div>
                 </div>
-                <div class = "row">
+                <div class="row">
                     <div class="col-sm-6">
-                        <label for="title" >Allergies</label>
+                        <label for="title">Allergies</label>
                         <input type="text" name="allergies" class="form-control"
                                value="<?php echo $allergies; ?>">
                     </div>
                     <div class="col-sm-6">
-                        <label for="title" >School</label>
+                        <label for="selectSchool">School</label>
                         <select name="selectSchool">
                             <?php
                             //Needs work done
@@ -196,15 +198,17 @@ if (isset($_POST["update"])) {
                         </select>
                     </div>
                 </div>
-                <div class = "row">
+                <div class="row">
                     <div class="col-sm-6">
-                        <label for="title">Parent/Guardian</label>
+                        <label for="selectParentGuardian">Parent/Guardian</label>
                         <select name="selectParentGuardian">
+                            <option value=NULL>None</option>
                             <?php
-                            //Currently working on this here (to also handle if the student has no parent/guardian listed)
-                            $queryParentGuardians = "SELECT guardianID, parentFName, parentLName FROM parentorguardian, student WHERE student.studentID = $studentID AND parentorguardian.guardianID = student.guardianID";
+                            //Currently working on this here (to also handle if the student has no parent/guardian listed).
+                            //Returns current parent or guardian of the student.
+                            $queryParentGuardians = "SELECT parentorguardian.guardianID, parentFName, parentLName FROM parentorguardian;";
                             $resultsFromParentGuardian = $database->query($queryParentGuardians);
-                            if ($resultsFromParentGuardian->num_rows == 1) {
+                            if ($resultsFromParentGuardian->num_rows > 0) {
                                 while ($parentGuardianResultSet = $resultsFromParentGuardian->fetch_assoc()) {
                                     ?>
                                 <option value="<?php echo $parentGuardianResultSet["guardianID"]; ?>">
@@ -214,19 +218,16 @@ if (isset($_POST["update"])) {
                             } else {
                                 echo "<option>There are no Parent/Guardians registered in STARS</option>";
                             }
+                            //Get current student's parent/guardian
+                            //SELECT parentorguardian.guardianID, parentFName, parentLName FROM parentorguardian, student WHERE
+                            //student.studentID = $studentID AND student.guardianID = parentorguardian.guardianID;
                             ?>
                         </select>
                     </div>
-                    <div class="col-sm-6">
-                        <label for="title">Username</label>
-                        <input type="text" name="username" class="form-control"
-                               value="<?php echo $_SESSION['username'] ?>" readonly>
-
-                    </div>
                 </div>
-                <div class = "row">
+                <div class="row">
                     <div class="col-sm-6">
-                        <label for="title">Support Educator</label>
+                        <label for="selectSupportEducator">Support Educator</label>
                         <select name="selectSupportEducator">
                             <option value=NULL>None</option>
                             <?php
@@ -251,34 +252,34 @@ if (isset($_POST["update"])) {
                     </div>
                 </div>
             </form>
-                </div>
-            </div>
+        </div>
+    </div>
 
-            <div class="bottom">
-                <div id="footer">
-                    <ul id="footerMenu">
-                        <li class="titleNav">List One
-                            <ul class="dropupMenu">
-                                <li><a>List 1:1</a></li>
-                                <li><a>List 1:2</a></li>
-                                <li><a>List 1:3</a></li>
-                                <li><a>List 1:4</a></li>
-                            </ul>
-                        </li>
-                        <li class="titleNav">List Two
-                            <ul class="dropupMenu">
-                                <li><a>List 2:1</a></li>
-                                <li><a>List 2:2</a></li>
-                                <li><a>List 2:3</a></li>
-                                <li><a>List 2:4</a></li>
-                            </ul>
-                        </li>
+    <div class="bottom">
+        <div id="footer">
+            <ul id="footerMenu">
+                <li class="titleNav">List One
+                    <ul class="dropupMenu">
+                        <li><a>List 1:1</a></li>
+                        <li><a>List 1:2</a></li>
+                        <li><a>List 1:3</a></li>
+                        <li><a>List 1:4</a></li>
                     </ul>
-                </div>
-            </div>
-        </body>
+                </li>
+                <li class="titleNav">List Two
+                    <ul class="dropupMenu">
+                        <li><a>List 2:1</a></li>
+                        <li><a>List 2:2</a></li>
+                        <li><a>List 2:3</a></li>
+                        <li><a>List 2:4</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </body>
     </html>
-<?php
+    <?php
 }
 //Close db connection
 $database->close();
