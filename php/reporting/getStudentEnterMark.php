6b846466b81203afbddb0f4135b61ee100143287
HC\jgaudet109873 <Jgaudet109873@hollandcollege.com>
2019-02-05 16:33:34 -0400

    Subject history changes.

--- php/reporting/getStudentEnterMark.php
+++ php/reporting/getStudentEnterMark.php
@@ -23,7 +23,7 @@ if (isset($_POST['classID'])) {
             AND student.studentID = enrollment.studentID;";
     $res = mysqli_query($database, $sql);
     if (mysqli_num_rows($res) > 0) {
-        echo "<option value=''>------- Select --------</option>";
+        echo "<option value=''>Select</option>";
         while ($row = mysqli_fetch_object($res)) {
             echo "<option value='" . $row->studentID . "'>" . $row->firstName . " " . $row->lastName . "</option>";
         }
