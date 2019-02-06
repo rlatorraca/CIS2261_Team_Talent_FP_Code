ae9ec10de7dca30a016416752af5bfb220313def
Sara Courtney <scourtney@hollandcollege.com>
2019-02-06 17:08:37 -0400

    about page

--- php/about.php
+++ php/about.php
@@ -8,6 +8,8 @@
      */
 
     session_start();
+    include "button.class.php";
+    $confirm = new Button();
 ?>
 
 <!doctype html>
@@ -24,6 +26,12 @@
         <!--Link to custom style sheet-->
         <link href="../css/stars.css" rel="stylesheet">
 
+        <!-- JQuery Links !-->
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/resources/demos/style.css">
+        <!-- JQuery Calendar Date Picker !-->
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
         <!-- Here is where we call bootstrap. !-->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
@@ -31,11 +39,14 @@
                 crossorigin="anonymous"></script>
 
 
-        <title>STARS - About</title>
+        <title>STARS</title>
     </head>
     <body>
 
-        <?php include "../php/about.php"; ?>
+
+
+        <?php include "../header.php"; ?>
+
 
         <div class="jumbotron-fluid">
             <div class="container-fluid">
@@ -44,11 +55,20 @@
                 <div class="home">
                     <h1 class="homeTitle">Welcome to STARS</h1>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt
-                       lore te feugait nulla facilisi. Nam liber tempor cum soluta
+                        ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+                        tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum
+                        iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu
+                        feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent
+                        luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta
                         nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim
                         assum.</p>
                 </div>
-
+                <!--                Login button-->
+                <!--                <div class="col-md-12">-->
+                <!---->
+                <!--                    <a href="php/login/login.php" class="btn button2 button" role="button">Login</a>-->
+                <!---->
+                <!--                </div>-->
             </div>
         </div>
 
