fe441eafeae2d0310e8bd6ecf085017243900b79
Rodrigo L S Pires <rlatorraca@gmail.com>
2019-02-14 17:27:26 -0400

    Added files and started forms

--- php/admin/confirmEditStudent.php
+++ php/admin/confirmEditStudent.php
@@ -1,83 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * Company: Team Talent 2.0
- * Members: John, Rodrigo, Sara, Steve
- * Date: 2/14/2019
- *
- * Page which confirms the database update query and handles errors for the user. Also handles refreshes and redirects.
- *
- * Required pages: stars.css, login.php, checkLoggedIn.php, autheticateAdminPages.php, searchStudent.php, editStudent.php.
- */
-
-//Lock down page
-include "../login/checkLoggedIn.php";
-
-//Admin only
-include "../login/authenticateAdminPages.php";
-
-//Database connection
-include "../db/dbConn.php";
-
-//Button class
-include "../button.class.php";
-
-//Header file
-include "../../header.php";
-
-if (isset($_POST["updateStudent"])) {
-
-    if ($_POST["firstName"] == "" || $_POST["middleName"] == "" || $_POST["lastName"] == "" ||
-        $_POST["gender"] == "" || $_POST["dob"] == "" || $_POST["address"] == "" || $_POST["phoneNum"] == "" ||
-        $_POST["emailAddress"] == "" || strlen($_POST["phoneNum"]) < 10 || strlen($_POST["phoneNum"]) > 10 ||
-        strlen($_POST["dob"]) < 10 || strlen($_POST["dob"]) > 10) {
-
-        $msg = "<br><div class='alert alert-danger'>Student could not be updated. Please make sure all fields are filled before updating.</div>";
-
-    } else {
-
-        $studentIDFromForm = $_GET["studentID"];
-
-//Compiling and cleaning user inputs
-        $firstNameFromForm = $database->real_escape_string($_POST["firstName"]);
-        $middleNameFromForm = $database->real_escape_string($_POST["middleName"]);
-        $lastNameFromForm = $database->real_escape_string($_POST["lastName"]);
-        $genderFromForm = $database->real_escape_string($_POST["gender"]);
-        $dobFromForm = $database->real_escape_string($_POST["dob"]);
-        $gradeFromForm = $database->real_escape_string($_POST["grade"]);
-        $addressFromForm = $database->real_escape_string($_POST["address"]);
-        $phoneNumberFromForm = $database->real_escape_string($_POST["phoneNum"]);
-        $emailAddressFromForm = $database->real_escape_string($_POST["emailAddress"]);
-        $allergiesFromForm = $database->real_escape_string($_POST["allergies"]);
-        $schoolIDFromForm = $database->real_escape_string($_POST["selectSchool"]);
-        $parentGuardianIDFromForm = $database->real_escape_string($_POST["selectParentGuardian"]);
-
-        $updateStudentQuery = "UPDATE student SET firstName = '$firstNameFromForm', middleName = '$middleNameFromForm', 
-                           lastName = '$lastNameFromForm', gender = '$genderFromForm', dob = '$dobFromForm', grade = '$gradeFromForm',
-                           address = '$addressFromForm', phoneNum = '$phoneNumberFromForm', emailAddress = '$emailAddressFromForm',
-                           allergies = '$allergiesFromForm', schoolID = $schoolIDFromForm, guardianID = $parentGuardianIDFromForm 
-                           WHERE studentID = $studentIDFromForm;";
-
-        $resultSetFromUpdateStudent = $database->query($updateStudentQuery);
-
-        if ($resultSetFromUpdateStudent == 1) {
-
-            $msg = "<br><div class='alert alert-info'>Student $studentIDFromForm has been successfully updated in STARS. 
-                        You will be automatically redirected to the Search Student page momentarily.</div><br>";
-
-            header("Refresh:5; searchStudent.php", true, 303);
-
-        } else {
-
-            $msg = "<br><div class='alert alert-danger'>Sorry, student $studentIDFromForm could not be updated to the database at this time</div><br>";
-
-            header("Refresh:5; javascript:history.back(-1)", true, 303);
-
-        }
-
-    }
-}
-?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -114,26 +34,108 @@ if (isset($_POST["updateStudent"])) {
     </script>
     <title>STARS - Confirm Edit Student</title>
 </head>
+<?php
+/**
+ * Created by PhpStorm.
+ * Company: Team Talent 2.0
+ * Members: John, Rodrigo, Sara, Steve
+ * Date: 2/14/2019
+ *
+ * Page which confirms the database update query and handles errors for the user. Also handles refreshes and redirects.
+ *
+ * Required pages: stars.css, login.php, checkLoggedIn.php, autheticateAdminPages.php, searchStudent.php, editStudent.php.
+ */
+
+
+//Lock down page
+include "../login/checkLoggedIn.php";
+
+//Admin only
+include "../login/authenticateAdminPages.php";
+
+//Database connection
+include "../db/dbConn.php";
+
+//Button class
+include "../button.class.php";
+//Header file
+include "../../header.php";
+
+
+if (isset($_POST["updateStudent"])) {
+
+	if ($_POST["firstName"] == "" || $_POST["middleName"] == "" || $_POST["lastName"] == "" ||
+		$_POST["gender"] == "" || $_POST["dob"] == "" || $_POST["address"] == "" || $_POST["phoneNum"] == "" ||
+		$_POST["emailAddress"] == "" || strlen($_POST["phoneNum"]) < 10 || strlen($_POST["phoneNum"]) > 10 ||
+		strlen($_POST["dob"]) < 10 || strlen($_POST["dob"]) > 10) {
+
+		$msg = "<br><div class='alert alert-danger'>Student could not be updated. Please make sure all fields are filled before updating.</div>";
+
+	} else {
+
+		$studentIDFromForm = $_GET["studentID"];
+
+//Compiling and cleaning user inputs
+		$firstNameFromForm = $database->real_escape_string($_POST["firstName"]);
+		$middleNameFromForm = $database->real_escape_string($_POST["middleName"]);
+		$lastNameFromForm = $database->real_escape_string($_POST["lastName"]);
+		$genderFromForm = $database->real_escape_string($_POST["gender"]);
+		$dobFromForm = $database->real_escape_string($_POST["dob"]);
+		$gradeFromForm = $database->real_escape_string($_POST["grade"]);
+		$addressFromForm = $database->real_escape_string($_POST["address"]);
+		$phoneNumberFromForm = $database->real_escape_string($_POST["phoneNum"]);
+		$emailAddressFromForm = $database->real_escape_string($_POST["emailAddress"]);
+		$allergiesFromForm = $database->real_escape_string($_POST["allergies"]);
+		$schoolIDFromForm = $database->real_escape_string($_POST["selectSchool"]);
+		$parentGuardianIDFromForm = $database->real_escape_string($_POST["selectParentGuardian"]);
+
+		$updateStudentQuery = "UPDATE student SET firstName = '$firstNameFromForm', middleName = '$middleNameFromForm', 
+                           lastName = '$lastNameFromForm', gender = '$genderFromForm', dob = '$dobFromForm', grade = '$gradeFromForm',
+                           address = '$addressFromForm', phoneNum = '$phoneNumberFromForm', emailAddress = '$emailAddressFromForm',
+                           allergies = '$allergiesFromForm', schoolID = $schoolIDFromForm, guardianID = $parentGuardianIDFromForm 
+                           WHERE studentID = $studentIDFromForm;";
+
+		$resultSetFromUpdateStudent = $database->query($updateStudentQuery);
+
+		if ($resultSetFromUpdateStudent == 1) {
+
+			$msg = "<br><div class='alert alert-info'>Student $studentIDFromForm has been successfully updated in STARS. 
+                        You will be automatically redirected to the Search Student page momentarily.</div><br>";
+
+			header("Refresh:5; searchStudent.php", true, 303);
+
+		} else {
+
+			$msg = "<br><div class='alert alert-danger'>Sorry, student $studentIDFromForm could not be updated to the database at this time</div><br>";
+
+			header("Refresh:5; javascript:history.back(-1)", true, 303);
+
+		}
+
+	}
+}
+?>
+
 <body>
 <div class="jumbotron-fluid">
     <div class="container-fluid">
         <div class="container"><?php if (isset($msg)) {
-                echo $msg;
-            }
-            //Back Button
-            $goBack = new Button();
-            $goBack->buttonName = "goBack";
-            $goBack->buttonID = "goBack";
-            $goBack->buttonValue = "Go Back";
-            $goBack->buttonStyle = "font-family:sans-serif";
-            $goBack->buttonWeb = "javascript:history.back(-1);";
-            $goBack->display(); ?></div>
+				echo $msg;
+			}
+			//Back Button
+			$goBack = new Button();
+			$goBack->buttonName = "goBack";
+			$goBack->buttonID = "goBack";
+			$goBack->buttonValue = "Go Back";
+			$goBack->buttonStyle = "font-family:sans-serif";
+			$goBack->buttonWeb = "javascript:history.back(-1);";
+			$goBack->display(); ?></div>
 
     </div>
 </div>
 <div class="bottom">
     <div id="footer">
-        <?php include("../../navMenu.php"); ?>
+		<?php include("../../navMenu.php"); ?>
     </div>
 </div>
 </body>
