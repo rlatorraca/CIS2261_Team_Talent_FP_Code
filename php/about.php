05cc39a04d0f732a4554aad4fd6bc2a50452f158
Sara Courtney <scourtney@hollandcollege.com>
2019-02-14 12:43:24 -0400

    clean-up

--- php/about.php
+++ php/about.php
@@ -37,12 +37,14 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
                 integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
                 crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
         <title>STARS</title>
     </head>
     <body>
 
         <?php include "../header.php"; ?>
+
         <div class="jumbotron-fluid">
             <div class="container-fluid container-sizer">
                 <div>
